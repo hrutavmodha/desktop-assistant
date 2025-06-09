6418e8b74c03eaf65e372d0bf14d8f0e73b5a3dc
Hrutav Modha <modhahrutav@gmail.com>
2025-06-09 21:36:09 +0530

    Update renderer.js

--- frontend/chat/renderer.js
+++ frontend/chat/renderer.js
@@ -3,13 +3,12 @@ const userText = document.getElementById("userText")
 const botReply = document.getElementById("botReply")
 startBtn.addEventListener("click", async () => {
   const { exec } = require("child_process")
-  exec(`python ../../backend/model/inference.py ${userText}`, (err, stdout, stderr) => {
+  exec(`python ../../backend/model/inference.py ${userText.innerText}`, (err, stdout, stderr) => {
     if (err) {
       botReply.innerText = "Error occurred"
       console.log(err)
       return
     }
-    userText.innerText = "Open YouTube"
     botReply.innerText = stdout.trim()
   })
 })
