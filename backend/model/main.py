c1003ee7afd987abc13cb80bd4d2807e1b05a8b7
Hrutav Modha <modhahrutav@gmail.com>
2025-06-10 18:20:01 +0530

    Update main.py

--- backend/model/main.py
+++ backend/model/main.py
@@ -3,10 +3,19 @@ import json
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 X = y = []
+OS = ""
+if sys.platform == "win32":
+    OS = "windows"
+elif sys.platform == "linux":
+    OS = "linux"
+elif sys.platform == "darwin":
+    OS = "mac"
+else:
+    print("The Operating System of your device is currently not supported. I am sorry")
 with open("data.json", "r") as file:
     data = json.load(file)
     for entry in data:
-        output_command = entry["output"]["windows"]  # change for other OS
+        output_command = entry["output"][OS]
         for variant in entry["input"]:
             X.append(variant)
             y.append(output_command)
