2f4fe94778e96e86b1c67620ee57412abeac6428
Hrutav Modha <modhahrutav@gmail.com>
2025-06-11 14:38:10 +0530

    Update wikiSearch.py

--- backend/model/handlers/wikiSearch.py
+++ backend/model/handlers/wikiSearch.py
@@ -4,7 +4,7 @@ def wikiSearch(query):
         summary = wikipedia.summary(query, sentences = 2)
         print(summary)
     except wikipedia.exceptions.DisambiguationError as e:
-        print(f"I am confused. It has several meanings like {e[:5]. Please specify")
+        print(f"I am confused. It has several meanings like {e[:5]}. Please specify")
     except wikipedia.exceptions.PageError:
         print("Can't find any information related to your query. Did you meant something else")
     except Exception as e:
