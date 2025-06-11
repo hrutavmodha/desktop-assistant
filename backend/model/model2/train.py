a0e49c44a7f6af73a47652d7dcc529b9478ef68f
Hrutav Modha <modhahrutav@gmail.com>
2025-06-11 22:53:15 +0530

    Update train.py

--- backend/model/model2/train.py
+++ backend/model/model2/train.py
@@ -4,11 +4,23 @@ from sklearn.linear_model import LogisticRegression
 from joblib import dump as save
 with open("data.json") as file:
     data = load(file)
+pssample = {
+    "<query>": ["dogs", "AI", "news", "time machine", "game"],
+    "<duration>": ["10 seconds", "2 minutes", "30 seconds", "5 mins", "1 hour"]
+}
 X, y = [], []
 for entry in data:
-    for sentence in entry["input"]:
-        X.append(sentence.lower())
-        y.append(entry["output"])
+    intent = entry["output"]
+    for template in entry["input"]:
+        for placeholder, samples in pssample.items():
+            if placeholder in template:
+                for sample in samples:
+                    X.append(template.replace(placeholder, sample))
+                    y.append(intent)
+                break
+        else:
+            X.append(template)
+            y.append(intent)
 vectorizer = CountVectorizer()
 X_vec = vectorizer.fit_transform(X)
 clf = LogisticRegression()
