c4b3abde64a40a6324e7b5225b7ef3e164f849d1
Hrutav Modha <modhahrutav@gmail.com>
2025-06-11 23:27:07 +0530

    Update inference.py

--- backend/model/model4/inference.py
+++ backend/model/model4/inference.py
@@ -1,10 +1,10 @@
 from joblib import load
 clf = load("model.pkl")
 vectorizer = load("vectorizer.pkl")
-def get_model_route(query: str) -> str:
+def get_model_route(query):
     query_vec = vectorizer.transform([query.lower()])
     return clf.predict(query_vec)[0]
 if __name__ == "__main__":
     user_query = input("You: ")
     selected_model = get_model_route(user_query)
-    print(f"Route this query to: {selected_model}")
+    print(f"Chosen model for this query: {selected_model}")
