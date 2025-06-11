c3be8c3a8973cba0a1ead0e4e493afec97788bb0
Hrutav Modha <modhahrutav@gmail.com>
2025-06-11 23:44:25 +0530

    Update inference.py

--- backend/model/model2/inference.py
+++ backend/model/model2/inference.py
@@ -1,8 +1,8 @@
-from joblib import load
-import sys
-clf = load("model.pkl")
-vectorizer = load("vectorizer.pkl")
-cmd = input("You: ").lower()
-cmd_vec = vectorizer.transform([cmd])
-intent = clf.predict(cmd_vec)[0]
-print(f"Predicted intent: {intent}")
+def execDynamicCMD(cmd):
+  from joblib import load
+  import sys
+  clf = load("model.pkl")
+  vectorizer = load("vectorizer.pkl")
+  cmd_vec = vectorizer.transform([cmd])
+  intent = clf.predict(cmd_vec)[0]
+  print(f"Predicted intent: {intent}")
