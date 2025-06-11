34058c44dec55a48397d0cfa0c04c10b888803b9
Hrutav Modha <modhahrutav@gmail.com>
2025-06-11 23:57:30 +0530

    Create main.py

---
+++ backend/model/main.py
@@ -0,0 +1,21 @@
+import sys
+from model1.inference import execShellCMD
+from model2.inference import execDynamicCMD
+from model4.inference import getModel
+def main():
+    if len(sys.argv) < 2:
+        print("No command provided!")
+        return
+    cmd = sys.argv[1].lower()
+    model = getModel(cmd)
+    print(f"Predicted model: {model}")
+    if route == "model1":
+        execShellCMD(cmd)
+    elif route == "model2":
+        execDynamicCMD(cmd)
+    elif route == "model3":
+        print("Model 3 not yet integrated.")
+    else:
+        print("Could not determine model.")
+if __name__ == "__main__":
+    main()
