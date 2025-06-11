a252316a903447dd6abf957faedc4b338fbc4d8d
Hrutav Modha <modhahrutav@gmail.com>
2025-06-11 21:24:34 +0530

    Update inference.py

--- backend/model/model1/inference.py
+++ backend/model/model1/inference.py
@@ -1,3 +1,6 @@
+from joblib import load
+model = load("model1.pkl")
+vectorizer = load("vectorizer1.pkl")
 user_vec = vectorizer.transform([user_input])
 predicted_command = model.predict(user_vec)[0]
 print("Understood your command. Processing it, it may take few moment. Please wait")
