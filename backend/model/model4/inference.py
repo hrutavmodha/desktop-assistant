c6f0a15722d277e28c06ad32246b7d6a1b6eed46
Hrutav Modha <modhahrutav@gmail.com>
2025-06-12 15:01:23 +0530

    Update inference.py

--- backend/model/model4/inference.py
+++ backend/model/model4/inference.py
@@ -1,6 +1,8 @@
-def getModel(query):
-    from joblib import load
-    clf = load("backend/model/model4/model4.pkl")
-    vectorizer = load("backend/model/model4/vectorizer4.pkl")
-    query_vec = vectorizer.transform([query.lower()])
-    return clf.predict(query_vec)[0]
+#def getModel(query):
+from joblib import load
+clf = load("backend/model/model4/model4.pkl")
+vectorizer = load("backend/model/model4/vectorizer4.pkl")
+query = sys.argv[1]
+query_vec = vectorizer.transform([query.lower()])
+model = clf.predict(query_vec)[0]
+print(model)
