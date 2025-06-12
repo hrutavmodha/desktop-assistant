0f664c3d769d6ffb859ccba39ac6c283e4205d12
Hrutav Modha <modhahrutav@gmail.com>
2025-06-12 15:12:34 +0530

    Update inference.py

--- backend/model/model2/inference.py
+++ backend/model/model2/inference.py
@@ -2,8 +2,8 @@
 from joblib import load
 import sys
 cmd = sys.argv[1]
-clf = load("backend/model/model2/model.pkl")
-vectorizer = load("backend/model/model2/vectorizer.pkl")
+clf = load("backend/model/model2/model2.pkl")
+vectorizer = load("backend/model/model2/vectorizer2.pkl")
 cmd_vec = vectorizer.transform([cmd])
 intent = clf.predict(cmd_vec)[0]
 print(f"Predicted intent: {intent}")
