a6e8277ba8e8826980c98be5400d968b4bb8691e
Hrutav Modha <modhahrutav@gmail.com>
2025-06-12 14:59:02 +0530

    Update train.py

--- backend/model/model4/train.py
+++ backend/model/model4/train.py
@@ -2,7 +2,7 @@ import json
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from joblib import dump
-with open("data.json") as f:
+with open("backend/model/model4/data.json") as f:
     data = json.load(f)
 X, y = [], []
 for item in data:
@@ -13,6 +13,6 @@ vectorizer = CountVectorizer()
 X_vec = vectorizer.fit_transform(X)
 clf = LogisticRegression()
 clf.fit(X_vec, y)
-dump(clf, "model.pkl")
-dump(vectorizer, "vectorizer.pkl")
+dump(clf, "backend/model/model4/model4.pkl")
+dump(vectorizer, "backend/model/model4/vectorizer4.pkl")
 print("Model-4 and vectorizer-4 are saved successfully")
