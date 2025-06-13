78aa0874eb7c75583e9b7465b905d854476e2a59
Hrutav Modha <modhahrutav@gmail.com>
2025-06-13 11:58:21 +0530

    Update inference.py

--- backend/model/model2/inference.py
+++ backend/model/model2/inference.py
@@ -1,11 +1,24 @@
 def execDynamicCMD(cmd):
+  import nltk
+  from nltk import word_tokenize, pos_tag
   from joblib import load
   import sys
   from handlers.googleSearch import googleSearch
+  nltk.download("punkt")
+  nltk.download("averaged_perceptron_tagger")
   clf = load("backend/model/model2/model2.pkl")
   vectorizer = load("backend/model/model2/vectorizer2.pkl")
   cmd_vec = vectorizer.transform([cmd])
   intent = clf.predict(cmd_vec)[0]
-  print(f"Predicted intent: {intent}")
+  term = ""
+  tokens = word_tokenize(cmd)
+  tagged = pos_tag(tokens) 
+  for word, tag in tagged:
+      if tag == "NNP":
+          term = term + " " + word
   if intent == "search":
-    googleSearch(cmd)
+      googleSearch(term)
+  elif intent == "wiki":
+      wikiSearch(term)
+  else:
+      print("Sorry, I can't find any relevant information")
