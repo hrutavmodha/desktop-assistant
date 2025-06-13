853b9383b0c5c117026c7a998c93c28eb9a586ad
Hrutav Modha <modhahrutav@gmail.com>
2025-06-13 22:42:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,19 @@
 const express = require("express")
 const cors = require("cors")
 const path = require("path")
+const handleSignup = require("./middlewares/auth")
+const handleDownload = require("./middlewares/download")
+const connect = require("./connect")
 require("dotenv").config()
 const app = express()
+app.use(express.json())
 app.use(cors({
-  origin: process.env.REACT_APP_FRONTEND_URL
+  origin: process.env.REACT_APP_FRONTEND_URL,
+  credentials: true,
 }))
-app.get("/download", (req, res) => {
-    const file = path.join(__dirname, "dist")
-    res.download(file, "VoiceAssistant.exe", (error) => {
-        if (error) {
-            console.log(error)
-            res.status(500).send("Internal server error. Please try again later")
-        }
-    })
-})
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-    console.log(`Download Server running on port ${PORT}`)
+connect("voice-assistant")
+app.get("/download", handleDownload)
+app.post("/signup", handleSignup)
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
 })
