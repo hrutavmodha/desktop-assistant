b69fb733273ad9621cdefeba87b783290530f6be
Hrutav Modha <modhahrutav@gmail.com>
2025-06-13 11:31:48 +0530

    Delete backend/model/handlers/searchGoogle.py

--- backend/model/handlers/searchGoogle.py
+++
@@ -1,5 +0,0 @@
-import webbrowser as web
-from urllib.parse import quote_plus as encodeurl
-def googleSearch(query):
-    search = encodeurl(uquery)
-    web.open(f"https://www.google.com/search?q={search}")
