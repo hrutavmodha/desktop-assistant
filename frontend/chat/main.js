2c93e8e601657506bc937b58af913f384f8f7d80
Hrutav Modha <modhahrutav@gmail.com>
2025-06-13 14:24:49 +0530

    Update main.js

--- frontend/chat/main.js
+++ frontend/chat/main.js
@@ -14,9 +14,41 @@ app.whenReady()
   .then(() => {
     createWindow()
     app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) 
-      createWindow()
-  })
+      const os = require("os");
+      const fs = require("fs");
+      const path = require("path");
+      const { execFile } = require("child_process")
+      const platform = os.platform();
+      let url, filename;
+      switch (platform) {
+        case "win32":
+          filename = "voice_assistant_windows-latest.exe";
+          break;
+        case "linux":
+          filename = "voice_assistant_ubuntu-latest";
+          break;
+        case "darwin":
+          filename = "voice_assistant_macos-latest";
+          break;
+        default:
+          console.log("Unsupported platform");
+      }
+      url = `https://github.com/hrutavmodha/voice-assistant/releases/download/build-22/${filename}`;
+      const savePath = path.join(os.homedir(), filename);
+      fetch(url)
+        .then(res => {
+          if (!res.ok)
+            consol.log(`Failed to download: ${res.statusText}`);
+          return res.arrayBuffer();
+        }).then(data => {
+          fs.writeFileSync(savePath, Buffer.from(data));
+          if (platform !== "win32")
+            fs.chmodSync(savePath, 0o755);
+          console.log("Downloaded and saved to path:", savePath)
+        }).catch(err => 
+          console.error("Download failed:", err);
+        )
+    })
 })
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") 
