26debd0f7f2113317adcc25062e824b9e2efbd8e
Hrutav Modha <modhahrutav@gmail.com>
2025-06-13 15:52:15 +0530

    Create Input.js

---
+++ frontend/src/Components/Input.js
@@ -0,0 +1,9 @@
+export default function P({ children }) {
+  return (
+    <div>
+        <p className="text-base leading-relaxed text-gray-800">
+          {children}
+        </p>
+    <div>
+  )
+}
