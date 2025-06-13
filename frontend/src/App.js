77e29118ed8c45105daa2d74a38fce5aa60a5e07
Hrutav Modha <modhahrutav@gmail.com>
2025-06-13 16:42:41 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,16 @@
-
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./Components/Home"
+import Download from "./components/Download"
+import Docs from "./Components/Docs"
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/download" element={<Download />} />
+        <Route path="/docs" element={<Docs />} />
+      </Routes>
+    </Router>
+  )
+}
