6083619676d64e6ca9317fa245961e73d490a871
Hrutav Modha <modhahrutav@gmail.com>
2025-06-13 18:01:58 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Download from "./Pages/Download"
 import About from "./Pages/About"
 import Contact from "./Pages/Contact"
+import Documentation from "./Pages/Docs"
 import NavBar from "./Components/NavBar"
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Download/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
+        <Route path="/documentation" element={<Documentation/>}/>
       </Routes>
     </Router>
   )
