863b429eb53e5dcc3be732f2dd1517addd2724c9
Hrutav Modha <modhahrutav@gmail.com>
2025-06-13 11:28:41 +0530

    Update inference.py

--- backend/model/model1/inference.py
+++ backend/model/model1/inference.py
@@ -1,10 +1,9 @@
-#def execShellCMD():
-from joblib import load
-import sys
-import os
-model = load("backend/model/model1/model.pkl")
-vectorizer = load("backend/model/model1/vectorizer.pkl")
-user_input = sys.argv[1].lower()
-user_vec = vectorizer.transform([user_input])
-predicted_command = model.predict(user_vec)[0]
-print(f"Understood your command. Processing it, it may take few moment. Please wait. This is predicted command: {predicted_command}")
+def execShellCMD(cmd):
+  from joblib import load
+  import sys
+  import os
+  model = load("backend/model/model1/model.pkl")
+  vectorizer = load("backend/model/model1/vectorizer.pkl")
+  user_vec = vectorizer.transform([cmd])
+  predicted_command = model.predict(user_vec)[0]
+  print(f"Understood your command. Processing it, it may take few moment. Please wait.")
