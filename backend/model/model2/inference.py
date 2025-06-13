7fd510f53878bc32bf88abac8f39fa74ac886574
Hrutav Modha <modhahrutav@gmail.com>
2025-06-13 11:31:06 +0530

    Update inference.py

--- backend/model/model2/inference.py
+++ backend/model/model2/inference.py
@@ -1,9 +1,10 @@
-#def execDynamicCMD(cmd):
-from joblib import load
-import sys
-cmd = sys.argv[1]
-clf = load("backend/model/model2/model2.pkl")
-vectorizer = load("backend/model/model2/vectorizer2.pkl")
-cmd_vec = vectorizer.transform([cmd])
-intent = clf.predict(cmd_vec)[0]
-print(f"Predicted intent: {intent}")
+def execDynamicCMD(cmd):
+  from joblib import load
+  import sys
+  clf = load("backend/model/model2/model2.pkl")
+  vectorizer = load("backend/model/model2/vectorizer2.pkl")
+  cmd_vec = vectorizer.transform([cmd])
+  intent = clf.predict(cmd_vec)[0]
+  print(f"Predicted intent: {intent}")
+  if intent == "search":
+    pass
