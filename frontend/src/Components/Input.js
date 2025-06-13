5e070eea7b81837b15ac0ea56556dc1a0d031e5a
Hrutav Modha <modhahrutav@gmail.com>
2025-06-13 15:56:01 +0530

    Update and rename Input.js to Paragraph.js

--- frontend/src/Components/Input.js
+++
@@ -1,9 +0,0 @@
-export default function P({ children }) {
-  return (
-    <div>
-        <p className="text-base leading-relaxed text-gray-800">
-          {children}
-        </p>
-    <div>
-  )
-}
---
+++ frontend/src/Components/Paragraph.js
@@ -0,0 +1,9 @@
+export default function P({ children }) {
+    return (
+        <>
+            <p className="text-base leading-relaxed text-gray-800">
+                {children}
+            </p>
+        </>
+    )
+}
