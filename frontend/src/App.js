39413cd7e4302fbea227d6428ae4c71d542b5a50
Hrutav Modha <modhahrutav@gmail.com>
2025-06-14 11:55:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Download from "./Pages/Download"
 import About from "./Pages/About"
 import Contact from "./Pages/Contact"
 import Documentation from "./Pages/Docs"
+import Signup from "./Pages/Signup"
 import NavBar from "./Components/NavBar"
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/documentation" element={<Documentation/>}/>
+        <Route path="/signup" element={<Signup/>}/>
       </Routes>
     </Router>
   )
